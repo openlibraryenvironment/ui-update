e68b82200bfa8b505ffeb9e25663ebd3bdcedcb2
Jason Skomorowski <jason@indexdata.com>
2019-11-18 18:26:08 -0500

    Towards MCL based brief display PR-278
    Outstanding are skeleton fields and red background for faulted rows.

--- src/routes/ScanRoute.js
+++ src/routes/ScanRoute.js
@@ -4,9 +4,19 @@ import { FormattedMessage } from 'react-intl';
 import { Form, Field } from 'react-final-form';
 import _ from 'lodash';
 import classNames from 'classnames';
-import { Layout, Row, Col, Select, TextField, List, Card } from '@folio/stripes/components';
+import { Layout, Row, Col, Select, TextField, MultiColumnList, Card, Icon } from '@folio/stripes/components';
 import { stripesConnect } from '@folio/stripes/core';
 import css from './ScanRoute.css';
+
+const scanFormatter = { success: scan => {
+  if (scan.success === true) {
+    return <Icon size="large" icon="check-circle" status="success" />;
+  }
+  if (scan.success === false) {
+    return <Icon size="large" icon="times-circle-solid" status="error" />;
+  }
+  return <Icon size="large" icon="clock" />;
+} };
 
 const ScanRoute = props => {
   // Unfortunately, it doesn't seem to be readily possible to initialise at the field level.
@@ -20,6 +30,7 @@ const ScanRoute = props => {
   const [selScan, setSelScan] = useState(null);
   const [scans, setScans] = useState([]);
   const [scanData, setScanData] = useState({});
+
   const onSubmit = (values, form) => {
     const scannedAt = Date.now();
     form.initialize(_.omit(values, 'reqId'));
@@ -33,6 +44,24 @@ const ScanRoute = props => {
       .GET({ path: `rs/patronrequests/${values.reqId}` })
       .then(res => setScanData(prevData => ({ ...prevData, [scannedAt]: res })));
   };
+
+  const formattedScans = scans.map(scan => {
+    const data = scanData[scan.scannedAt];
+    if (!data) {
+      return { success: null, reqId: scan.reqId, requester: '', supplier: '', title: '' };
+    }
+    const mayGet = prop => _.get(data, prop, '');
+    console.log(scan, data);
+    return {
+      success: true,
+      reqId: scan.reqId,
+      requester: mayGet('requestingInstitutionSymbol'),
+      supplier: mayGet('supplyingInstitutionSymbol'),
+      title: mayGet('title'),
+      scannedAt: scan.scannedAt,
+    };
+  });
+
   return (
     <Layout className="padding-all-gutter">
       <Row>
@@ -68,29 +97,33 @@ const ScanRoute = props => {
               </form>
             )}
           />
-          <List
-            items={scans}
-            itemFormatter={req => {
-              const mayGet = prop => _.get(scanData[req.scannedAt], prop, '');
-              return (
-                <button
-                  type="button"
-                  onClick={() => setSelScan(req.scannedAt)}
-                  key={req.scannedAt}
-                  className={css.scanned}
-                >
-                  <Card
-                    cardStyle="positive"
-                    cardClass={classNames({ [css.selected]: req.scannedAt === selScan })}
-                    roundedBorder
-                    headerStart={mayGet('title') || req.reqId}
-                  >
-                    {mayGet('isRequester') ? 'Loan' : 'Return'}
-                  </Card>
-                </button>
-              );
-            }}
-          />
+          {formattedScans.length > 0 &&
+            <MultiColumnList
+              contentData={formattedScans}
+              formatter={scanFormatter}
+              visibleColumns={['success', 'reqId', 'requester', 'supplier', 'title']}
+              isSelected={({ item }) => selScan === item.scannedAt}
+              onRowClick={(e, row) => setSelScan(row.scannedAt)}
+              columnMapping={{
+                success: '',
+                reqId: <FormattedMessage id="ui-scan.column.reqId" />,
+                requester: <FormattedMessage id="ui-scan.column.requester" />,
+                supplier: <FormattedMessage id="ui-scan.column.supplier" />,
+                title: <FormattedMessage id="ui-scan.column.title" />
+              }}
+              // It would be better to express the fixed-width columns in em and let
+              // the remaining space go to Title, alas there seems to be an MCL bug
+              // preventing this. Adding up to less than 100% is necessary to avoid
+              // scrollbars introduced by some negative margin somewhere.
+              columnWidths={{
+                success: '5%',
+                reqId: '30%',
+                requester: '10%',
+                supplier: '10%',
+                title: '44%',
+              }}
+            />
+          }
         </Col>
         <Col xs={4}>
           {selScan === null && 'Scan an item!'}
