5d22c89fd9f1e86eeb5bae8f0d956ca912d266cf
Jason Skomorowski <jason@indexdata.com>
2024-08-30 15:12:48 -0400

    Local note PR-1890

--- src/components/ScanList.js
+++ src/components/ScanList.js
@@ -1,18 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { Icon, MultiColumnList } from '@folio/stripes/components';
+import { Icon, MultiColumnList, Tooltip } from '@folio/stripes/components';
 import STATUS from '../scanStatus';
 
-const scanFormatter = { status: scan => {
-  if (scan.status === STATUS.SUCCESS) {
-    return <Icon size="large" icon="check-circle" status="success" />;
-  }
-  if (scan.status === STATUS.FAIL) {
-    return <Icon size="large" icon="times-circle-solid" status="error" />;
-  }
-  return <Icon size="large" icon="clock" />;
-} };
+const scanFormatter = {
+  status: scan => {
+    if (scan.status === STATUS.SUCCESS) {
+      return <Icon size="large" icon="check-circle" status="success" />;
+    }
+    if (scan.status === STATUS.FAIL) {
+      return <Icon size="large" icon="times-circle-solid" status="error" />;
+    }
+    return <Icon size="large" icon="clock" />;
+  },
+  notes: scan => (
+    <>
+      {scan.notes?.localNote &&
+        <Tooltip
+          id="rs-local-note-tooltip"
+          text={<FormattedMessage id="stripes-reshare.hasLocalNote" />}
+        >
+          {({ ref, ariaIds }) => (
+            <Icon
+              icon="report"
+              aria-labelledby={ariaIds.text}
+              ref={ref}
+            />
+          )}
+        </Tooltip>
+      }
+      {scan.notes?.patronNote &&
+        <Tooltip
+          id="rs-patron-note-tooltip"
+          text={<FormattedMessage id="stripes-reshare.hasPatronNote" />}
+        >
+          {({ ref, ariaIds }) => (
+            <Icon
+              icon="profile"
+              aria-labelledby={ariaIds.text}
+              ref={ref}
+            />
+          )}
+        </Tooltip>
+      }
+    </>
+  )
+};
 
 const ScanList = ({ scans, scanData, selectedScan, onRowClick }) => {
   const formattedScans = scans.map(scannedAt => {
@@ -23,6 +57,7 @@ const ScanList = ({ scans, scanData, selectedScan, onRowClick }) => {
       requester: scan?.request?.requestingInstitutionSymbol ?? '',
       supplier: scan?.request?.supplyingInstitutionSymbol ?? '',
       title: scan?.request?.title ?? '',
+      notes: { localNote: scan?.request?.localNote, patronNote: scan?.request?.patronNote },
       scannedAt,
     };
   });
@@ -31,7 +66,7 @@ const ScanList = ({ scans, scanData, selectedScan, onRowClick }) => {
     <MultiColumnList
       contentData={formattedScans}
       formatter={scanFormatter}
-      visibleColumns={['status', 'hrid', 'requester', 'supplier', 'title']}
+      visibleColumns={['status', 'hrid', 'notes', 'requester', 'supplier', 'title']}
       isSelected={({ item }) => selectedScan === item.scannedAt}
       onRowClick={onRowClick}
       columnMapping={{
@@ -39,7 +74,8 @@ const ScanList = ({ scans, scanData, selectedScan, onRowClick }) => {
         hrid: <FormattedMessage id="ui-update.column.hrid" />,
         requester: <FormattedMessage id="ui-update.column.requester" />,
         supplier: <FormattedMessage id="ui-update.column.supplier" />,
-        title: <FormattedMessage id="ui-update.column.title" />
+        title: <FormattedMessage id="ui-update.column.title" />,
+        notes: ''
       }}
       // It would be better to express the fixed-width columns in em and let
       // the remaining space go to Title, alas there seems to be an MCL bug
@@ -48,6 +84,7 @@ const ScanList = ({ scans, scanData, selectedScan, onRowClick }) => {
       columnWidths={{
         status: { max: 40 },
         hrid: { max: 100 },
+        notes: { max: 48, min: 0 },
         requester: { min: 140 },
         supplier: { min: 140 },
       }}
